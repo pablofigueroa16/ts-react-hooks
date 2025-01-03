fbe8c57312fcccc7a45e064b40dd3521a4a65973
pablofigueroa16 <pablofigueroa611@gmail.com>
2025-01-02 23:16:56 -0300

    chore: configure ESLint and Prettier with updated settings and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,34 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['**/node_modules/**', '**/dist/**', '**/*.test.ts'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+  eslintPluginPrettier,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+]
