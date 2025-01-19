d58143cb7cd81d0a8c16dbdde31aa2270c6c0f00
pablofigueroa16 <pablofigueroa611@gmail.com>
2025-01-18 21:24:27 -0300

    feat: add Vite configuration for building lvlup-react-hooks library

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(new URL('src/index.ts', import.meta.url).pathname),
+      name: 'lvlup-react-hooks',
+    },
+    sourcemap: true,
+    minify: true,
+    target: 'es6',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      include: ['src/**/*'],
+      exclude: ['vite.config.ts', 'vitest.config.ts', '**/*.test.ts'],
+    }),
+  ],
+})
