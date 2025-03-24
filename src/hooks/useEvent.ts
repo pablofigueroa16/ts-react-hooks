c1c37072407655c34b4cf94c71c42321005f793f
pablofigueroa16 <pablofigueroa611@gmail.com>
2025-03-23 23:24:27 -0300

    feat: implement useEvent hook with callback handling

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -1,1 +1,10 @@
-export function useEvent() {}
+import * as React from 'react'
+export function useEvent(callback) {
+  // useRef + useCallback
+  const ref = React.useRef(() => {})
+  React.useEffect(() => {
+    ref.current = callback
+  }, [callback])
+
+  return React.useCallback((...args) => ref.current(...args), [])
+}
